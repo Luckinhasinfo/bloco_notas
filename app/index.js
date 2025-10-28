3abc449a98745d1566fd5a805754f4b9f159e249
Luckinhasinfo <lao9@aluno.ifnmg.edu.br>
2025-10-28 14:48:16 -0300

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, View, FlatList, Button } from 'react-native';
+import Login from './componente/login';
+import { use, useState } from 'react';
+import { Text, TextInput } from 'react-native-web';
+
+
+    <View>
+      <View>
+      </View>
+      <StatusBar style='auto'></StatusBar>
+    </View>
+
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
---
+++ componente/login.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Button, TextInput, View} from "react-native"
+
+
+const Login = () => {
+}
+
+
+const styles = StyleSheet.create({
+
+  inputText:{
+    
+  },
+});
+
+export default Login;
